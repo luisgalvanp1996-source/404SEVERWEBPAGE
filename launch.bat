071eaeb8e3c5b816fdcf8819f4578a8ee9b8ef75
luisgalvanp1996-source <luisgalvanp1996@gmail.com>
2025-12-03 02:17:17 -0800

    Implement code changes to enhance functionality and improve performance

--- launch.bat
+++ launch.bat
@@ -1,3 +1,23 @@
 @echo off
-"C:\Users\Administrador\AppData\Local\Programs\Python\Python314\python.exe" "C:\Python\404SEVERWEBPAGE\app.py" --host=0.0.0.0 --port=5001
+
+:: Ruta donde está el BAT
+set "BASE_DIR=%~dp0"
+
+:: Carpeta DATA\logs junto al BAT
+set "LOG_DIR=%BASE_DIR%DATA\logs"
+set "LOG=%LOG_DIR%\server.log"
+
+:: Crear carpeta DATA\logs si no existe
+if not exist "%LOG_DIR%" (
+    mkdir "%LOG_DIR%"
+)
+
+echo =============================== >> "%LOG%"
+echo Inicio: %date% %time% >> "%LOG%"
+echo =============================== >> "%LOG%"
+
+"C:\Users\Administrador\AppData\Local\Programs\Python\Python314\python.exe" ^
+ "%BASE_DIR%app.py" --host=0.0.0.0 --port=5001 >> "%LOG%" 2>&1
+
+echo Servidor finalizado: %date% %time% >> "%LOG%"
 pause
